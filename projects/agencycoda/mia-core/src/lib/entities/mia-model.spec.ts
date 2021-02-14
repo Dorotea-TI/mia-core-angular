ac6b3a0488a813d55d3a8770db70c455478f699b
Matias Camiletti <matias.camiletti@gmail.com>
2021-02-14 00:22:01 -0300

    Add model class.

---
+++ projects/agencycoda/mia-core/src/lib/entities/mia-model.spec.ts
@@ -0,0 +1,7 @@
+import { MiaModel } from './mia-model';
+
+describe('MiaModel', () => {
+  it('should create an instance', () => {
+    expect(new MiaModel()).toBeTruthy();
+  });
+});
--- projects/agencycoda/mia-core/src/public-api.ts
+++ projects/agencycoda/mia-core/src/public-api.ts
@@ -6,6 +6,7 @@ export * from './lib/entities/mia-error';
 export * from './lib/entities/mia-response';
 export * from './lib/entities/mia-pagination';
 export * from './lib/entities/mia-query';
+export * from './lib/entities/mia-model';
 
 export * from './lib/services/mia-base-http.service';
 export * from './lib/services/mia-base-crud-http.service';
